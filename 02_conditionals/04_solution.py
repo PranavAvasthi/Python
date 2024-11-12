6f5e2aae138d66a54a6755b2c7652cbddbccede5
PranavAvasthi <pranavavasthi44@gmail.com>
2024-11-12 12:20:28 +0530

    Conditionals Solutions

---
+++ 02_conditionals/04_solution.py
@@ -0,0 +1,12 @@
+fruit = "Banana"
+color = "Yellow"
+
+if fruit == "Banana":
+    if color == "Green":
+        print("Unripe")
+    elif color == "Yellow":
+        print("Ripe")
+    elif color == "Brown":
+        print("OverRipe")
+else:
+    print("Information for this fruit is not available")
---
+++ 02_conditionals/07_solution.py
@@ -0,0 +1,8 @@
+order_size = "Medium"
+extra_shot = True
+if extra_shot:
+    coffee = order_size + " Coffee with an Extra Shot"
+else:
+    coffee = order_size + " Coffee"
+    
+print("Order:", coffee)
